a08dceec42550ad2793c4b8673798f98e9bbf1f4
mckinler1639 <47669828+mckinler1639@users.noreply.github.com>
2019-02-15 11:24:53 -0500

    Add files via upload

---
+++ P2HW1_PoundsKilograms_RobertMckinley.py
@@ -0,0 +1,15 @@
+# Convert pounds into kilograms
+# 15 February 2019
+# CTI-110 P2HWI - Pounds to Kilograms Converter
+# Robert Mckinley
+#
+# ask the user to enter a number
+pounds=int(input("how many pounds you want to convert?"))
+# Convert the formula
+kg=pounds/2.2046
+# Display the converted number
+# put in code for two decimal places
+print(format (kg,".2f"),"kilograms")
+
+
+
---
+++ P2HW2_MealTip_RobertMckinley.py
@@ -0,0 +1,13 @@
+# Calculate the total amount of meals purchased
+# 15 February 2019
+# CTI-110 P2HW2- Meal Tip Calculator
+# Robert Mckinley
+# 
+# Enter the total amount for meal
+meal= float(input("Please enter the total amount of the meal in dollars."))
+# calculate the tip percentage of 10%
+print( "10% of meal",format (meal*.10, ".2f"))
+# calculate the tip percentage of 18%
+print("18% of meal",format (meal*.18, ".2f"))
+# calculate the tip percentage of 20%
+print("20% of meal",format (meal*.20, ".2f"))
